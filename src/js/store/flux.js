3d0d9efdfb3b5219c6bcb9865652264a69a8f219
AslanSN <aslan.staub@manumartinez.es>
2022-06-08 00:20:56 +0200

    Initial docs setted

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,18 +1,9 @@
+import data from "../../resources/mockData.json"
+
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			data: [...data]
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
--- webpack.common.js
+++ webpack.common.js
@@ -40,7 +40,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-        favicon: '4geeks.ico',
+        favicon: 'Arup_Red_RGB_Initial.ico',
         template: 'template.html'
     }),
     new Dotenv({ safe: true, systemvars: true })
